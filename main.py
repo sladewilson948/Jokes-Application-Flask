2d24dd176bba76da58da7292907e488a80288ff5
Aman Dubey <67802423+sladewilson948@users.noreply.github.com>
2022-11-21 21:43:24 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+import requests
+
+
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET","POST"])
+def index():
+    final_list = get_jokes()
+    return render_template("index.html", data=final_list)
+
+
+def get_jokes():
+    list_of_jokes = []
+    for _ in range(10):
+        r = requests.get("https://joke.deno.dev")
+        data = r.json()
+        list_of_jokes.append([data["setup"], data["punchline"]])
+    
+    return list_of_jokes
+
+
+
+if __name__ == "__main__":
+    app.run()
